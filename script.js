785b128503d50f047f148913516c23b13bab934b
unknown <capchajennyfer4@gmail.comgit config --global user.email capchajennyfer4@gmail.com>
2020-11-02 11:50:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+for (i=0; i<names; i++)
+console.log(names[i]);
+if (name=j)
+console.log("Goodbye")
+else
+   (name = m)
+ console.log("Hello");  
+ 
+  
+function holamundo(){
+  alert("Hello, Goodbye");
+}
+
